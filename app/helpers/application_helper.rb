44296114a2391a139773d108f27f631a8c4a9417
gregmalcolm <greg@gregmalcolm.com>
2010-10-24 12:53:53 -0400

    Wrote some tests for galleries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
 	#for shared/_menu.html.erb
 	#
 	def obtain_sub_pages(curr_page, all_pages)
-		return [] if curr_page.nil? or all_pages.nil?
+		return [] if curr_page.blank? or all_pages.blank?
     
 		pid = curr_page.parent_id.nil? ? curr_page.id : curr_page.parent_id
 		all_pages.reject {|p| p.parent_id != pid }
