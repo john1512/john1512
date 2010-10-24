921dc3380bca6e9a50c66a01cd55a029668fa347
Doug Alcorn <dougalcorn@gmail.com>
2010-10-23 23:23:32 -0400

    explicitly require helps engine

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Refinery
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/vendor/engines/helps/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -40,3 +41,5 @@ module Refinery
     config.filter_parameters += [:password]
   end
 end
+
+require 'helps'
